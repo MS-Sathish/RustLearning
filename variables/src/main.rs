24ff92247e6ef7f68b49f49e7c6aeac9cfa841c5
sathish <sathish@kalakendradao.org>
2024-04-05 17:54:47 +0530

    Updated

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -3,4 +3,32 @@ fn main() {
     println!("The value of number is {num}");
     num = 7;
     println!("The value of number is {num}");
+
+    //integer
+    let a: i32 = 23;
+    println!("{a}");
+    let a: isize = 23;
+    println!("{a}");
+
+    //float 
+    let b: f64 = 2.34;
+    println!("{b}");
+
+    //bool
+    let c:bool = false;
+    println!("{c}");
+
+    const arr:[i32;5] = [1,2,3,4,4];
+    let first = arr[0];
+    println!("{}",first);
+
+
+    let x:(i32,f64,u8) = (500, 6.4, 1);
+    let five = x.0;
+    println!("{}",five);
+
+    for number in (1..4) {
+        println!("{number}!");
+    }
+
 }
